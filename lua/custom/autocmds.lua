a6bd126a6b02d0e195bf6b36bbc32e13138e8394
Neyens Jari <jari.neyens@telenetgroup.be>
2023-11-23 14:21:40 +0100

    add black & prettier

--- lua/custom/autocmds.lua
+++ lua/custom/autocmds.lua
@@ -20,21 +20,22 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 	pattern = { "*.yaml", "*.yml" },
 	desc = "Auto-format YAML files after saving",
 	callback = function()
-		local fileName = vim.api.nvim_buf_get_name(0)
-		vim.cmd(":silent !yamlfmt " .. fileName)
+		-- local fileName = vim.api.nvim_buf_get_name(0)
+		-- vim.cmd(":silent !yamlfmt " .. fileName)
+		vim.cmd(":silent !prettier . --write")
 	end,
 	group = autocmd_group,
 })
 
 
--- vim.api.nvim_create_autocmd({ "BufWritePost" }, {
--- 	pattern = { "*.py" },
--- 	desc = "Auto-format Python files after saving",
--- 	callback = function()
--- 		local fileName = vim.api.nvim_buf_get_name(0)
--- 		vim.cmd(":silent !black --preview -q " .. fileName)
--- 		vim.cmd(":silent !isort --profile black --float-to-top -q " .. fileName)
--- 		vim.cmd(":silent !docformatter --in-place --black " .. fileName)
--- 	end,
--- 	group = autocmd_group,
--- })
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	pattern = { "*.py" },
+	desc = "Auto-format Python files after saving",
+	callback = function()
+		local fileName = vim.api.nvim_buf_get_name(0)
+		vim.cmd(":silent !black --preview -q " .. fileName)
+		-- vim.cmd(":silent !isort --profile black --float-to-top -q " .. fileName)
+		-- vim.cmd(":silent !docformatter --in-place --black " .. fileName)
+	end,
+	group = autocmd_group,
+})
